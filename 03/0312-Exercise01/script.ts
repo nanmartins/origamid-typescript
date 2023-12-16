83a0f2e4d37b0d53a9d4b0ad3ef4ba0300a516c1
Renan Martins <renanmartins989@gmail.com>
2023-12-16 14:45:31 +0000

    Challenge

---
+++ 03/0312-Exercise01/script.ts
@@ -0,0 +1,10 @@
+console.log('connected...')
+document.body.style.background = '#333'
+
+// 1 - Crie uma interface UserData para o formulário abaixo
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+// 3 - Adicione um evento de keyup ao formulário
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+// 5 - Salve UserData no localStorage
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
